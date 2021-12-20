7d290c0b1ebcc934d571193da8268cc71ceaccfa
Zachary Elliott <zacharyberdell@gmail.com>
2021-12-20 15:23:50 -0700

    Built the index for the models.

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Users = require('./User');
+const Blogs = require('./Blogs');
+const Comments = require('./Comments')
+
+Users.hasMany(Blogs, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blogs.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+Users.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Blogs, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+Blogs.hasMany(Comments, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { Users, Blogs, Comments };
